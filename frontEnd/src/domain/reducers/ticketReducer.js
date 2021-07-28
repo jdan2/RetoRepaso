6808d0497b3d5740f766df31f3f087cee83054ef
JesusL <thedarkness2555@gmail.com>
2021-07-27 22:25:41 -0500

    Se importan los tipos para el crud de tiquete y se crean los casos en el reducer para listar tiquetes

--- frontEnd/src/domain/reducers/ticketReducer.js
+++ frontEnd/src/domain/reducers/ticketReducer.js
@@ -2,6 +2,14 @@ import {
     ADD_TICKET,
     ADD_TICKET_FAILURE,
     ADD_TICKET_SUCCESS,
+    LIST_TICKETS,
+    LIST_TICKETS_SUCCESS,
+    LIST_TICKETS_FAILURE,
+    DELETE_TICKET,
+    DELETE_TICKET_SUCCESS,
+    DELETE_TICKET_FAILURE,
+    ONLY_TICKET,
+    EDIT_TICKET
 } from '../types/ticket.js';
 
 //Creamos el estado inicial
@@ -30,6 +38,23 @@ const ticketsReducer =(state=initialState, action) =>{
                 ...state,
                 loading:false,
                 error: action.payload
+            }
+        case LIST_TICKETS:
+            return{
+                ...state,
+                loading:action.payload
+            }
+        case LIST_TICKETS_SUCCESS:
+            return{
+                ...state,
+                loading:false,
+                tickets: action.payload
+            }
+        case LIST_TICKETS_FAILURE:
+            return{
+             ...state,
+             loading:false,
+             error: action.payload
             }
             default:
             return state;

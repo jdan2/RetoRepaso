47577a9b27375ff821c352af403451e8e8f835da
Santiago Alarcon <santiago-alar@hotmail.com>
2021-07-28 13:44:15 -0500

    Fix error with cors, status 200 was not returned

--- backNodeJs/lib/interface/tiquete/routes/TiqueteRoute.js
+++ backNodeJs/lib/interface/tiquete/routes/TiqueteRoute.js
@@ -7,30 +7,30 @@ router.post('/', async(req, res)=>{
 
     req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
     const tiquete = await TiqueteController.createTiquete(req);
-    res.send(tiquete);
+    res.status(200).send(tiquete);
 });
 
 router.get('/', async(req, res)=>{
     req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
     if(req.query.id != undefined ){
         const tiquete = await TiqueteController.consultarTiquete(req);
-        res.send(tiquete);
+        res.status(200).send(tiquete);
     }else{
         const tiquetes = await TiqueteController.listarTiquetes(req);
-        res.send(tiquetes);
+        res.status(200).send(tiquetes);
     }
 });
 
 router.put('/', async(req, res)=>{
     req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
     const tiquete = await TiqueteController.editarTiquete(req);
-    res.send(tiquete);
+    res.status(200).send(tiquete);
 });
 
 router.delete('/', async(req, res)=>{
     req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
     const tiquete = await TiqueteController.eliminarTiquete(req);
-    res.send(tiquete);
+    res.status(200).send(tiquete);
 })
 
 module.exports = router;

92e4e23cec295e780cadeb89d36002359a4586dd
JesusL <thedarkness2555@gmail.com>
2021-07-27 19:40:35 -0500

    Se importan los tipos en las acciones del tiquete. Se crea la accion para listar tiquetes

--- frontEnd/src/domain/actions/ticketActions.js
+++ frontEnd/src/domain/actions/ticketActions.js
@@ -3,6 +3,18 @@ import {
     ADD_TICKET,
     ADD_TICKET_FAILURE,
     ADD_TICKET_SUCCESS,
+
+    //LIST
+    LIST_TICKETS,
+    LIST_TICKETS_SUCCESS,
+    LIST_TICKETS_FAILURE,
+    //DELETE
+    DELETE_TICKET,
+    DELETE_TICKET_SUCCESS,
+    DELETE_TICKET_FAILURE,
+    //READ
+    ONLY_TICKET,
+    EDIT_TICKET,
 } from '../types/ticket.js';
 
 
@@ -38,3 +50,34 @@ const addTicketFailure = (error) =>({
     type:ADD_TICKET_FAILURE,
     payload:error
 });
+
+//Listar tickets
+export function listTicketsAction(){
+    return async(dispatch) =>{
+        dispatch(listTickets());
+        try{
+            //Hago mi peticion HTTP
+            const response = await clientAxios.get('/listartiquetes');
+            dispatch(listTicketsSuccess(response.data));
+
+        }catch(error){
+            dispatch(listTicketsFailure());
+        }
+    }
+}
+
+const listTickets = () =>({
+    type:LIST_TICKETS,
+    payload:true
+})
+
+const listTicketsSuccess = (tickets) =>({
+    type:LIST_TICKETS_SUCCESS,
+    payload:tickets
+})
+
+const listTicketsFailure = () =>({
+    type:LIST_TICKETS_FAILURE,
+    payload:true
+})
+

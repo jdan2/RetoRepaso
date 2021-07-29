040eb339ea235d4f87e753e19ea6bb4798935d2d
JesusL <thedarkness2555@gmail.com>
2021-07-29 00:37:58 -0500

    Se enlaza el tiquete con la generacion de la neuva factura

--- frontEnd/src/presentation/pages/NewFactura.js
+++ frontEnd/src/presentation/pages/NewFactura.js
@@ -1,10 +1,19 @@
 import React, {useState} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 //Acciones de redux
 import {addNewFacturaAction} from '../../domain/actions/facturaActions'; 
+import { auth } from '../../infrastructure/services/firebase/firebase';
 
 const NewFactura = () =>{
+
+    const ticket = useSelector(
+        (state) => state.ticketReducer.ticketone
+      );
+
+      const { celdaId, tipoVehiculo, placa, horaIngreso, tiqueteId} =
+  ticket;
+  console.log(ticket);
 
     //Datos de mi formulario
     const [id, setId] = useState('');
@@ -42,19 +51,59 @@ const NewFactura = () =>{
     <div>
         <h2>Agregar Factura</h2>
         <form onSubmit={submitAddFactura}>
-            Id
+            Id Factura
             <input 
                 type="text"
                 name="id"
                 value={id}
                 onChange={e => setId(e.target.value)}    
             />
+            Id Tiquete
+            <input 
+                type="text"
+                name="tiqueteId"
+                value={tiqueteId}
+                disabled   
+            />
+            Celda Asignada
+            <input 
+                type="text"
+                name="celdaId"
+                value={celdaId}
+                disabled   
+            />
+
+            Placa Vehiculo
+            <input 
+                type="text"
+                name="placa"
+                value={placa}
+                disabled   
+            />
             Id del empleado
              <input 
                 type="text"
                 name="empleadoId"
-                value={empleadoId}
-                onChange={e => setEmpleadoId(e.target.value)}    
+                value={auth().currentUser.email}
+                  
+                disabled 
+            />
+
+            Tipo de Vehiculo
+             <input 
+                type="text"
+                name="tipoVehiculo"
+                value={tipoVehiculo}
+                 
+                disabled 
+            />
+            Hora de Ingreso
+             <input 
+                type="text"
+                name="horaIngreso"
+                value={horaIngreso}
+                disabled
+                    
             />
             Hora de Salida
              <input 

66b10ad3dc69a8175962aec8a5542e05a8604761
Santiago Alarcon <santiago-alar@hotmail.com>
2021-07-28 19:48:21 -0500

    Try & Catch para cada enrutamiento de Route

--- backNodeJs/lib/interface/tiquete/routes/TiqueteRoute.js
+++ backNodeJs/lib/interface/tiquete/routes/TiqueteRoute.js
@@ -5,32 +5,56 @@ const TiqueteController = require('../controllers/TiqueteController')
 
 router.post('/', async(req, res)=>{
 
-    req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
-    const tiquete = await TiqueteController.createTiquete(req);
-    res.status(200).send(tiquete);
+    try{
+        req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
+        const tiquete = await TiqueteController.createTiquete(req);
+        res.status(200).send(tiquete);
+    }catch(err){
+        console.log(err)
+        res.status(500);
+    }  
+   
 });
 
 router.get('/', async(req, res)=>{
-    req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
-    if(req.query.id != undefined ){
-        const tiquete = await TiqueteController.consultarTiquete(req);
-        res.status(200).send(tiquete);
-    }else{
-        const tiquetes = await TiqueteController.listarTiquetes(req);
-        res.status(200).send(tiquetes);
-    }
+
+    try{
+        req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
+        if(req.query.id != undefined ){
+            const tiquete = await TiqueteController.consultarTiquete(req);
+            res.status(200).send(tiquete);
+        }else{
+            const tiquetes = await TiqueteController.listarTiquetes(req);
+            res.status(200).send(tiquetes);
+        }
+    }catch(err){
+        console.log(err)
+        res.status(500);
+    } 
+
 });
 
 router.put('/', async(req, res)=>{
-    req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
-    const tiquete = await TiqueteController.editarTiquete(req);
-    res.status(200).send(tiquete);
+    try{
+        req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
+        const tiquete = await TiqueteController.editarTiquete(req);
+        res.status(200).send(tiquete);
+    }catch(err){
+        console.log(err)
+        res.status(500);
+    }
+
 });
 
 router.delete('/', async(req, res)=>{
-    req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
-    const tiquete = await TiqueteController.eliminarTiquete(req);
-    res.status(200).send(tiquete);
+    try{
+        req.app.serviceLocator = require('../../../infrastructure/config/service-locator');
+        const tiquete = await TiqueteController.eliminarTiquete(req);
+        res.status(200).send(tiquete);
+    }catch(err){
+        console.log(err)
+        res.status(500);
+    }
 })
 
 module.exports = router;

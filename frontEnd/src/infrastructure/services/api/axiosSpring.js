06e63e4b0d8fa2b41a5e47a9ec1d0321d7f36710
JesusL <thedarkness2555@gmail.com>
2021-07-29 21:48:27 -0500

    Se agrega URL definitiva del backend de spring

--- frontEnd/src/infrastructure/services/api/axiosSpring.js
+++ frontEnd/src/infrastructure/services/api/axiosSpring.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-    baseURL: 'http://localhost:8080/api/'
+    baseURL: 'https://glacial-everglades-61490.herokuapp.com/api/'
 });
 
 export default clientAxios;

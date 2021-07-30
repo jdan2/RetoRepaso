72dcd6c0820c22e8f0bdda1603ea6d28d16fde1e
daniel <juandortiz.15@gmail.com>
2021-07-30 01:28:02 -0500

    se crea metodo en agregado para valor total

--- backSpring/domain/model/src/main/java/co/com/sofka/model/factura/gateways/FacturaRepository.java
+++ backSpring/domain/model/src/main/java/co/com/sofka/model/factura/gateways/FacturaRepository.java
@@ -3,7 +3,9 @@ package co.com.sofka.model.factura.gateways;
 import co.com.sofka.model.factura.Factura;
 import co.com.sofka.model.factura.values.CanitdadMinutos;
 import co.com.sofka.model.factura.values.HoraSalida;
+import co.com.sofka.model.factura.values.ValorTotal;
 import co.com.sofka.model.tiquete.values.HoraIngreso;
+import co.com.sofka.model.tiquete.values.TipoVehiculo;
 
 import java.util.List;
 
@@ -15,5 +17,5 @@ public interface FacturaRepository {
     Factura updateFactura(Factura factura);
     void deletefactura(String idFactura);
     CanitdadMinutos canitdadMinutos(HoraIngreso horaIngreso, HoraSalida horaSalida);
-
+    ValorTotal valorTotal (TipoVehiculo tipoVehiculo, CanitdadMinutos canitdadMinutos);
 }

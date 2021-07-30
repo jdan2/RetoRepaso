cb851ef4a0c400d459d39a781b2b9c51be94983b
Santiago Alarcon <santiago-alar@hotmail.com>
2021-07-29 23:53:47 -0500

    Se crea el servicio encontrarFacturasPorEmpleadoId en DBRepository

--- backSpring/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/mongo/MongoFacturaDBRepository.java
+++ backSpring/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/mongo/MongoFacturaDBRepository.java
@@ -1,8 +1,13 @@
 package co.com.sofka.mongo;
 
+import co.com.sofka.model.factura.Factura;
+import co.com.sofka.model.factura.values.EmpleadoId;
 import co.com.sofka.mongo.entities.FacturaEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 
+import java.util.List;
+
 public interface MongoFacturaDBRepository extends MongoRepository<FacturaEntity, String>, QueryByExampleExecutor<FacturaEntity> {
+    List<Factura> findFacturaByEmpleadoId(EmpleadoId empleadoId);
 }

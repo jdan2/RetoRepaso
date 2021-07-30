7a266a091fb8ad6fc695f09eb72e7ab5d08c6b43
Santiago Alarcon <santiago-alar@hotmail.com>
2021-07-29 23:51:53 -0500

    Se agrega el metodo findFacturaByEmpleadoId en el repositorio

--- backSpring/domain/model/src/main/java/co/com/sofka/model/factura/gateways/FacturaRepository.java
+++ backSpring/domain/model/src/main/java/co/com/sofka/model/factura/gateways/FacturaRepository.java
@@ -2,6 +2,7 @@ package co.com.sofka.model.factura.gateways;
 
 import co.com.sofka.model.factura.Factura;
 import co.com.sofka.model.factura.values.CanitdadMinutos;
+import co.com.sofka.model.factura.values.EmpleadoId;
 import co.com.sofka.model.factura.values.HoraSalida;
 import co.com.sofka.model.tiquete.values.HoraIngreso;
 
@@ -15,4 +16,5 @@ public interface FacturaRepository {
     Factura updateFactura(Factura factura);
     void deletefactura(String idFactura);
     CanitdadMinutos canitdadMinutos(HoraIngreso horaIngreso, HoraSalida horaSalida);
+    List<Factura> findFacturaByEmpleadoId(EmpleadoId empleadoId);
 }

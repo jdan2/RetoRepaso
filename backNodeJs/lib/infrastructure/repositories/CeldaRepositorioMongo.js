5abc64fe2477c542c483edf293e4edc24ba7592d
Santiago Alarcon <santiago-alar@hotmail.com>
2021-08-01 16:08:04 -0500

    Se crea el metodo en el repo para la busqueda de celda por id

--- backNodeJs/lib/infrastructure/repositories/CeldaRepositorioMongo.js
+++ backNodeJs/lib/infrastructure/repositories/CeldaRepositorioMongo.js
@@ -19,16 +19,23 @@ module.exports = class extends CeldaRepository {
   }
 
   async get(celdaId){
-    const mongooseCelda = await MongooseCelda.findOne({celdaId: celdaId});
-    return new Celda(mongooseCelda.celdaId, mongooseCelda.disponibilidad, mongooseCelda.tipoCelda, (err, doc)=>{
-      if(err) throw new Error('ERR_CELDA_ID_PROVIDED_WAS_NOT_FOUND');
-    });
+    const mongooseCelda = await MongooseCelda.findOne({celdaId: celdaId}).exec();
+      if(!mongooseCelda) throw new Error('ERR_CELDA_ID_PROVIDED_WAS_NOT_FOUND');
+    
+    return new Celda(mongooseCelda.celdaId, mongooseCelda.disponibilidad, mongooseCelda.tipoCelda);
   }
 
   async modify(celdaEntity){
     const{celdaId, disponibilidad,tipoCelda} = celdaEntity;
     const mongooseCelda = await MongooseCelda.findOneAndUpdate({celdaId:celdaId},{disponibilidad:disponibilidad, tipoCelda:tipoCelda},{new:true});
     return new Celda(mongooseCelda.celdaId, mongooseCelda.disponibilidad, mongooseCelda.tipoCelda);
+  }
+
+  async getAvailableCeldas(){
+    const mongooseCeldas = await MongooseCelda.find({disponibilidad:  { $regex: "true"}});
+    return mongooseCeldas.map((mongooseCelda)=>{
+      return new Celda(mongooseCelda.celdaId, mongooseCelda.disponibilidad, mongooseCelda.tipoCelda)
+    })
   }
 
 };

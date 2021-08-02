faf9a112e9b219e2acc1415c3508bb69077e05ce
JesusL <thedarkness2555@gmail.com>
2021-08-01 23:47:51 -0500

    Se crean test para las acciones de tiquete

--- frontEnd/src/domain/test/domain.test.js
+++ frontEnd/src/domain/test/domain.test.js
@@ -4,6 +4,20 @@ import {
     addTicket,
     addTicketSuccess,
     addTicketFailure,
+    listTickets,
+    listTicketsSuccess,
+    listTicketsFailure,
+    listCeldas,
+    listCeldasSuccess,
+    listCeldasFailure,
+    deleteTicket,
+    deleteTicketSuccess,
+    deleteTicketFailure,
+    onlyTicket,
+    editarTicket,
+
+
+
     
 } from "../actions/ticketActions";  
 
@@ -17,6 +31,14 @@ describe('reducer tickets test functions', () => {
         celdaId: "C123",
         tipoVehiculo: "moto"
     }
+
+    const dummyCelda = {
+        celdaId:"C008",
+        disponibilidad: true,
+        tipoCelda: "carro"
+    
+    }
+
 
     const dummyFactura = {
         facturaId: "HoPQghuxLxfbMVHYAviqkTIk2JK2",
@@ -53,6 +75,84 @@ describe('reducer tickets test functions', () => {
     test('reducer ADD_TICKET_FAILURE case', () => {
       
         const action = addTicketFailure("Error");
+        const state = reducer(initialState, action);
+        expect(state).toEqual({...initialState, error: "Error"})
+        
+       // expect(reducer(state, action())).toEqual({"tickets":[], "error":null, "loading":true, "ticketone":null})
+    })
+
+    test('reducer LIST_TICKES case', () => {
+      
+        const action = listTickets(dummyTicket);
+        const state = reducer(initialState, action);
+        expect(state).toEqual({...initialState})
+        
+       // expect(reducer(state, action())).toEqual({"tickets":[], "error":null, "loading":true, "ticketone":null})
+    })
+
+    test('reducer LIST_TICKES_SUCESS case', () => {
+      
+        const action = listTicketsSuccess(dummyTicket);
+        const state = reducer(initialState, action);
+        expect(state).toEqual({...initialState, tickets: dummyTicket})
+        
+       // expect(reducer(state, action())).toEqual({"tickets":[], "error":null, "loading":true, "ticketone":null})
+    })
+    test('reducer LIST_TICKES_FAILURE case', () => {
+      
+        const action = listTicketsFailure("Error");
+        const state = reducer(initialState, action);
+        expect(state).toEqual({...initialState, error: "Error"})
+        
+       // expect(reducer(state, action())).toEqual({"tickets":[], "error":null, "loading":true, "ticketone":null})
+    })
+
+    test('reducer LIST_CELDAS case', () => {
+      
+        const action = listCeldas(dummyCelda);
+        const state = reducer(initialState, action);
+        expect(state).toEqual({...initialState})
+        
+       // expect(reducer(state, action())).toEqual({"tickets":[], "error":null, "loading":true, "ticketone":null})
+    })
+
+    test('reducer LIST_CELDAS_SUCESS case', () => {
+      
+        const action = listCeldasSuccess(dummyCelda);
+        const state = reducer(initialState, action);
+        expect(state).toEqual({...initialState, celdas: dummyCelda})
+        
+       // expect(reducer(state, action())).toEqual({"tickets":[], "error":null, "loading":true, "ticketone":null})
+    })
+    test('reducer LIST_CELDAS_FAILURE case', () => {
+      
+        const action = listCeldasFailure("Error");
+        const state = reducer(initialState, action);
+        expect(state).toEqual({...initialState, error: "Error"})
+        
+       // expect(reducer(state, action())).toEqual({"tickets":[], "error":null, "loading":true, "ticketone":null})
+    })
+
+    test('reducer DELETE_TICKET case', () => {
+      
+        const action = deleteTicket(dummyTicket.tiqueteId);
+        const state = reducer(initialState, action);
+        expect(state).toEqual({...initialState})
+        
+       // expect(reducer(state, action())).toEqual({"tickets":[], "error":null, "loading":true, "ticketone":null})
+    })
+
+    test('reducer DELETE_TICKET_SUCESS case', () => {
+      
+        const action = deleteTicketSuccess(dummyTicket);
+        const state = reducer(initialState, action);
+        expect(state).toEqual({...initialState, tickets: dummyTicket})
+        
+       // expect(reducer(state, action())).toEqual({"tickets":[], "error":null, "loading":true, "ticketone":null})
+    })
+    test('reducer DELETE_TICKET_FAILURE case', () => {
+      
+        const action = deleteTicketFailure("Error");
         const state = reducer(initialState, action);
         expect(state).toEqual({...initialState, error: "Error"})
         

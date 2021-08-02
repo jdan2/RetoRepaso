39d0624abb329fd60979ebad1fb33575de161833
Santiago Alarcon <santiago-alar@hotmail.com>
2021-08-02 00:57:43 -0500

    Modify test for Crear Celda

--- backNodeJs/__test__/crearCelda.spec.js
+++ backNodeJs/__test__/crearCelda.spec.js
@@ -3,21 +3,21 @@ const CeldaRepository = require('../lib/domain/tiquete/entities/CeldaRepository'
 const mockCeldaRepository = new CeldaRepository();
 const CrearCelda = require('../lib/application/celda/use_cases/CrearCelda');
 
-test('should resolve with the newly persisted celda', async()=>{
+describe('Crear una celda', ()=>{
+    test('should resolve with the newly persisted celda', async()=>{
 
-    //given
-    const persistedCelda = new Celda("C111k", "true", "doble");
-    mockCeldaRepository.persist = jest.fn(()=> persistedCelda);
-
-    //when
-    const celda = await CrearCelda("C111k", "true", "doble", {celdaRepository: mockCeldaRepository});
-
-    //expect
-    expect( mockCeldaRepository.persist).toHaveBeenCalledWith(new Celda("C111k", "true", "doble"));
-    expect(celda).toEqual(persistedCelda);
-    expect(celda.getCeldaId()).toEqual(persistedCelda.celdaId);
-    expect(celda.getDisponibilidad()).toEqual(persistedCelda.disponibilidad);
-    expect(celda.getTipoCelda()).toEqual(persistedCelda.tipoCelda);
-
-
-});
+        //given
+        const persistedCelda = new Celda("C111k", "true", "doble");
+        mockCeldaRepository.persist = jest.fn(()=> persistedCelda);
+    
+        //when
+        const celda = await CrearCelda("C111k", "true", "doble", {celdaRepository: mockCeldaRepository});
+    
+        //expect
+        expect( mockCeldaRepository.persist).toHaveBeenCalledWith(new Celda("C111k", "true", "doble"));
+        expect(celda).toEqual(persistedCelda);
+        expect(celda.getCeldaId()).toEqual(persistedCelda.celdaId);
+        expect(celda.getDisponibilidad()).toEqual(persistedCelda.disponibilidad);
+        expect(celda.getTipoCelda()).toEqual(persistedCelda.tipoCelda);
+    });
+})
